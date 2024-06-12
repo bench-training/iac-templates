bfc8548cdb93ab63520d82453542bbec717f003a
jinkeepaja <pj.jinkee@gmail.com>
2024-06-12 17:56:13 +0800

    Update main.tf

--- azure_tf/main.tf
+++ azure_tf/main.tf
@@ -1,1 +1,20 @@
+#  Example `main.tf`:
+# The configuration for the `remote` backend.
 
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.1"
+    }
+  }
+}
+
+provider "local" {
+  # Configuration options
+}
+
+resource "local_file" "sample_file" {
+  content  = "foo!"
+  filename = "${path.module}/foo.bar"
+}
