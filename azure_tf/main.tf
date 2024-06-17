fb5d1d3e410d09ae4311e53d6d4ee7ddb3da4b37
jinkeepaja <pj.jinkee@gmail.com>
2024-06-17 21:19:54 +0800

    Update main.tf

--- azure_tf/main.tf
+++ azure_tf/main.tf
@@ -20,6 +20,7 @@ terraform {
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 #  subscription_id   = "${{ secrets.AZURE_SUBSCRIPTION_ID }}"
 #  tenant_id         = "${{ secrets.AZURE_TENANT_ID }}"
 #  client_id         = "${{ secrets.AZURE_CLIENT_ID }}"
