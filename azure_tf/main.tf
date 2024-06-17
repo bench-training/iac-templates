93c5d2fe3f3c63d29e239fa8f332a1705e094c73
jinkeepaja <pj.jinkee@gmail.com>
2024-06-17 23:47:27 +0800

    Update main.tf

--- azure_tf/main.tf
+++ azure_tf/main.tf
@@ -8,26 +8,23 @@ terraform {
       version = "3.108.0"
     }
   }
-  backend "azurerm" {
-      resource_group_name  = "pj-jinkee"
-      storage_account_name = "pjjinkeesta"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
-      use_oidc             = true
-  }
 
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "pj-jinkee"
+    storage_account_name = "pjjinkeesta"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
 }
 
 provider "azurerm" {
   features {}
   use_oidc = true
-#  subscription_id   = "${{ secrets.AZURE_SUBSCRIPTION_ID }}"
-#  tenant_id         = "${{ secrets.AZURE_TENANT_ID }}"
-#  client_id         = "${{ secrets.AZURE_CLIENT_ID }}"
-#  client_secret     = "${{ secrets.AZURE_AD_CLIENT_SECRET }}"
 }
 
 resource "azurerm_resource_group" "state-demo-secure" {
   name     = "state-demo"
-  location = "southeast asia"
+  location = "southeastasia"
 }
